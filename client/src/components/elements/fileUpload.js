bf3391a07b00d4452c23b1db4122b20fb28065c8
cr1tterp0wer <capodacac@gmail.com>
2020-10-01 07:41:50 -0500

    master: file submit working

--- client/src/components/elements/fileUpload.js
+++ client/src/components/elements/fileUpload.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 
  export const FileUpload = () => {
+	const [fileName, setFileName] = useState('Choose File');
+
+	const sprungeUpload = (filename) => {
+	};
+
+	const fileInputChange = (evt) => {
+		evt.preventDefault();
+		console.log(evt.target.files[0]);
+		setFileName(evt.target.files[0].name);
+	};
+
+	const submitFile = (evt) => {
+		evt.preventDefault();
+		console.log(evt.target);
+	};
+
 	return (
 	<div id='FileUploader'>
-		<div className='input-group mb-3'>
-			<div className='custom-file'>
-				<input type='file' className='custom-file-input' id='inputGroupFile02'/>
-				<label className='custom-file-label' htmlFor='inputGroupFile02'>Choose file</label>
+		<form onSubmit={submitFile}>
+			<div className='input-group'>
+				<div className='input-group-prepend'>
+					<input className={(fileName=='Choose File') ? 'btn btn-secondary': 'btn btn-primary'} id='FileUploadButton' type='submit' value='Submit'/>
+				</div>
+				<div className='custom-file'>
+					<input
+					 type='file'
+					 className='custom-file-input'
+					 id='FileUploaderChoice'
+					 onChange={fileInputChange}
+					 aria-describedby='FileUploaderChoice'/>
+
+					<label className='custom-file-label' htmlFor='FileUploaderChoice'>{fileName}</label>
+				</div>
 			</div>
-			<div className='input-group-append'>
-				<span className='input-group-text' id='SubmitUpload'>Upload</span>
-			</div>
-		</div>
+		</form>
 	</div>
   );
 }
