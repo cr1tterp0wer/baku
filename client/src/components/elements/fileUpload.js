3f979e31be60aa6e0411038759842c4d680bf3ed
cr1tterp0wer <capodacac@gmail.com>
2020-10-03 14:28:13 -0500

    master: post to sprunge with node

--- client/src/components/elements/fileUpload.js
+++ client/src/components/elements/fileUpload.js
@@ -1,47 +1,14 @@
 import React, {useContext, useState} from 'react';
 import $ from 'jquery';
 import axios from 'axios';
+import {UploadContext} from '../../contexts/uploadContext';
 
  export const FileUpload = () => {
 	const [fileName, setFileName] = useState('Choose File');
 	const [file, setFile] = useState();
+	const {dataURL, setDataURL} = useContext(UploadContext);
 
-	let reader = new FileReader(),
-	    fileOutput = '';
-
-	reader.onload = function(e) {
-		fileOutput += reader.result;
-	}
-
-	reader.onloadend = function(e) {
-		postSprunge(fileOutput);
-	}
-
-	const postSprunge = async (data) => {
-		let formData = new FormData();
-		formData.append('file', file).then(function() {
-
-		console.log(formData);
-
-		});
-		const config = {
-			method: 'POST',
-			body: formData
-		};
-
-		fetch('/sprunge', config).then((response) => {
-			console.log(response);
-		});
-
-		/*
-		const response = await fetch('/sprunge', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ datapoints: data})
-		}).then(function(response) {
-			console.log(response);
-		});
-		*/
+	const postSprunge = (data) => {
 	};
 
 	const fileInputChange = (evt) => {
@@ -50,33 +17,39 @@ import axios from 'axios';
 		setFile(evt.target.files[0]);
 	};
 
-
 	//SEND TO BACKEND, WAIT FOR RESPONSE FROM SPRUNGE, SET THE DATAURL
 	const submitFile = (evt) => {
 		evt.preventDefault();
-		reader.readAsText(file);
+		const formData = new FormData();
+		formData.append('myFile', file, fileName);
+
+		axios.post('/sprunge', formData).then(function(sprungeURL) {
+			if (sprungeURL) {
+				setDataURL(sprungeURL.data);
+				console.log(dataURL);
+			}
+		});
 	};
 
 	return (
-	<div id='FileUploader'>
-		<form onSubmit={submitFile}>
-			<div className='input-group'>
-				<div className='input-group-prepend'>
-					<input className={(fileName=='Choose File') ? 'btn btn-secondary': 'btn btn-primary'} id='FileUploadButton' type='submit' value='Submit'/>
+		<div id='FileUploader'>
+			<form onSubmit={submitFile}>
+				<div className='input-group'>
+					<div className='input-group-prepend'>
+						<input className={(fileName=='Choose File') ? 'btn btn-secondary': 'btn btn-primary'} id='FileUploadButton' type='submit' value='Submit'/>
+					</div>
+					<div className='custom-file'>
+						<input
+						 type='file'
+						 className='custom-file-input'
+						 id='FileUploaderChoice'
+						 onChange={fileInputChange}
+						 aria-describedby='FileUploaderChoice'/>
+						<label className='custom-file-label' htmlFor='FileUploaderChoice'>{fileName}</label>
+					</div>
 				</div>
-				<div className='custom-file'>
-					<input
-					 type='file'
-					 className='custom-file-input'
-					 id='FileUploaderChoice'
-					 onChange={fileInputChange}
-					 aria-describedby='FileUploaderChoice'/>
-
-					<label className='custom-file-label' htmlFor='FileUploaderChoice'>{fileName}</label>
-				</div>
-			</div>
-		</form>
-	</div>
+			</form>
+		</div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -7,64 +7,88 @@
  * MIT License
  */
 
-const express = require('express');
 const PORT = 7777;
-const server = express();
-const cors = require('cors');
-const app = express();
 const axios = require('axios');
+const {Readable} = require('stream');
+var http = require('http');
+var inspect = require('util').inspect;
+var Busboy = require('busboy');
 var FormData = require('form-data');
 
-app.use(express.urlencoded({limit: '50mb'}));
-app.use(cors());
+const trySprunge = (fileData, callBack) => {
 
-/**
- * GET: /
- * Establish PING PONG
- * @params {Object} req - request from client
- * @params {Object} res - response to client
- */
-app.get('/', (req, res) => {
-	res.status(200).send('Success: Baku up on ' + PORT + '!');
-});
+	var formData = new FormData();
+	const fileBuffer = new Readable();
+	fileBuffer._read = () => {};
+	fileBuffer.push(fileData[0]);
+	fileBuffer.push(null);
 
-// Start the server
-app.listen(PORT, () => {
-	console.log('Baku has awakened on PORT:' + PORT + '!');
-});
-
-/**
- * POST: /sprunge
- * Post CSV to url: req.body.mediaURL
- * @params {Object} req - request from client
- * @params {Object} res - response to client
- */
-app.post('/sprunge', (req, res) => {
-	let file = req.file;
-	console.log(file);
-
-	/*
-	const form = new FormData();
-	form.append('buffer', datapoints);
-	form.append('content-type', 'text/csv');
-
-	console.log(form.getHeaders());
+	formData.append('sprunge', fileBuffer);
 
 	var config = {
 		method: 'post',
 		url: 'http://sprunge.us',
-		headers: form.getHeaders(),
-		body: form
+		headers: { 
+			'Accept': '*/*',
+			'Content-Type': 'multipart/form-data', 
+			'Connection':'Keep-Alive',
+			...formData.getHeaders()
+		},
+		data : formData
 	};
 
 	axios(config)
 	.then(function (response) {
-		console.log(response.data);
+		callBack(response.data);
 	})
 	.catch(function (error) {
+		callBack(error);
 		console.log(error);
 	});
-*/
-	res.status(200).send('Success: Sprunge sent!');
+};
+
+http.createServer(function(req, res) {
+	var base64data = [];
+
+	if (req.method === 'POST') {
+		var busboy = new Busboy({ headers: req.headers });
+
+		busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+			console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+			var buffer ='';
+			file.on('data', function(data) {
+				console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+				buffer+=data;
+			});
+
+			file.on('end', function() {
+				console.log('File [' + fieldname + '] Finished');
+				base64data.push(buffer);
+			});
+		});
+
+		busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+			console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+		});
+
+		busboy.on('finish', function() {
+
+			if (base64data.length > 0) {
+
+				trySprunge(base64data, function(sprunge) {
+					console.log(JSON.stringify(sprunge));
+					res.writeHead(200, { Connection: 'close'});
+					res.end(sprunge);
+				});
+			} else {
+				res.writeHead(303, { Connection: 'close', Location: '/' });
+				res.end();
+			}
+		});
+
+		req.pipe(busboy);
+	} 
+}).listen(PORT, function() {
+	console.log('Baku Server listening on '+PORT);
 });
 
